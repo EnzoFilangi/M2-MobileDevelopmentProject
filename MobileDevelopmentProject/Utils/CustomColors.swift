2e6762890bec917df70c5839df72227138894fe1
EnzoFilangi <enzo.filangi@efrei.net>
2022-12-14 11:54:45 +0100

    Extended Color to easily access the custom colors of the app

---
+++ MobileDevelopmentProject/Utils/CustomColors.swift
@@ -0,0 +1,17 @@
+//
+//  CustomColors.swift
+//  MobileDevelopmentProject
+//
+//  Created by Enzo Filangi on 14/12/2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let accent = Color(uiColor: UIColor(named: "AccentColor")!)
+    
+    static let main = Color(uiColor: UIColor(named: "Primary")!)
+    
+    static let background = Color(uiColor: UIColor(named: "Background")!)
+}
