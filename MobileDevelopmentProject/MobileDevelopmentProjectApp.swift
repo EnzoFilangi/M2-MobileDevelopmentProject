a8c70b8e797678d2cd657ad3040e17257295975b
EnzoFilangi <enzo.filangi@efrei.net>
2022-12-14 09:59:20 +0100

    Project initialization

---
+++ MobileDevelopmentProject/MobileDevelopmentProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileDevelopmentProjectApp.swift
+//  MobileDevelopmentProject
+//
+//  Created by Enzo Filangi on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MobileDevelopmentProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
