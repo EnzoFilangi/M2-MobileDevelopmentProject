a4f0ee120a4914ffc0d47a442e85011fc21f1686
EnzoFilangi <enzo.filangi@efrei.net>
2022-12-14 16:16:42 +0100

    Talk model

---
+++ MobileDevelopmentProject/Model/Talks/TalkModel.swift
@@ -0,0 +1,59 @@
+//
+//  TalkModel.swift
+//  MobileDevelopmentProject
+//
+//  Created by Enzo Filangi on 14/12/2022.
+//
+
+import Foundation
+import SwiftUI
+
+// MARK: - TalkAPIResponse
+struct TalkAPIResponse: Codable {
+    let records: [TalkRecord]
+    let offset: String
+}
+
+// MARK: - Record
+struct TalkRecord: Codable {
+    let id, createdTime: String
+    let fields: TalkFields
+}
+
+// MARK: - Fields
+struct TalkFields: Codable {
+    let location, activity, type: String
+    let start, end: Date
+    let speakers: [String]?
+
+    enum CodingKeys: String, CodingKey {
+        case end = "End"
+        case activity = "Activity"
+        case type = "Type"
+        case speakers = "Speaker(s)"
+        case start = "Start"
+        case location = "Location"
+    }
+}
+
+// MARK: - Convert event type to a Color
+struct TalkTypeColor {
+    static public func get(_ eventType: String) -> Color {
+        switch(eventType){
+        case "Panel":
+            return Color(red: 165/255, green: 198/255, blue: 250/255)
+        case "Keynote":
+            return Color(red: 243/255, green: 173/255, blue: 136/255)
+        case "Workshop":
+            return Color(red: 236/255, green: 161/255, blue: 222/255)
+        case "Meal":
+            return Color(red: 249/255, green: 216/255, blue: 126/255)
+        case "Breakout session":
+            return Color(red: 200/255, green: 177/255, blue: 250/255)
+        case "Networking":
+            return Color(red: 141/255, green: 219/255, blue: 196/255)
+        default:
+            return Color(white: 0.75)
+        }
+    }
+}
